4f5b8b0968baaf312f6a03aee96cd304a90b7caa
Lynn Htet <74451748+alexlynn013@users.noreply.github.com>
2022-05-29 22:16:04 +0630

    done

--- src/with_ide/Q23OutputFollowingFormat.java
+++ src/with_ide/Q23OutputFollowingFormat.java
@@ -2,10 +2,14 @@ package with_ide;
 
 public class Q23OutputFollowingFormat {
     public static void main(String[] args) {
-        int arrs[];
-        for (int i = 0; i < 5; i++) {
+        int arrs[] = {5,5,5,5};
+        for (int i = 0; i < arrs.length; i++) {
 
-
+            int z= arrs[i];
+            for (int j = 0; j<z; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
     }
 
