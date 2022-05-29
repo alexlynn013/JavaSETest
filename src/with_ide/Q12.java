3de354d118ee413c642b5c06d21d05de81244029
Lynn Htet <74451748+alexlynn013@users.noreply.github.com>
2022-05-29 21:24:13 +0630

    done

--- src/with_ide/Q12.java
+++ src/with_ide/Q12.java
@@ -3,19 +3,32 @@ package with_ide;
 import java.util.Scanner;
 
 public class Q12 {
-    public static void main(String[] args) {
-        int arry[] = { 1,2,3,4,5,5,6,7,8};
-        for (int i = 0; i < arry.length ; i++){
-            System.out.print(arry[i]+ "\t");
-        }
-        System.out.println();
-        System.out.println("Type your arrays index room that you want to delete");
-        Scanner obj = new Scanner(System.in);
-        int delenum = obj.nextInt();
 
-        switch (delenum){
-            case 0:
-                System.out.println();
+    public static int removeDuplicates(int array[], int n){
+        if(n==0 || n==1){
+            return n;
         }
+        int[] temp = new int[n];
+        int j = 0;
+        for(int i=0; i<n-1; i++){
+            if(array[i] != array[i+1]){
+                temp[j++] = array[i];
+            }
+        }
+        temp[j++] = array[n-1];
+
+        for(int i=0; i<j; i++){
+            array[i] = temp[i];
+        }
+        return j;
+    }
+    public static void main (String[] args) {
+        int array[] = {18,18,25,25,25,28,28,29};
+        int length = array.length;
+        length = removeDuplicates(array, length);
+
+        for(int i=0; i<length; i++)
+            System.out.print(array[i]+" ");
+
     }
 }
