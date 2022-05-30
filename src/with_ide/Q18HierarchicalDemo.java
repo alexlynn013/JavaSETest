31be1dca48d496b7568463c786d527675f6e86d0
Lynn Htet <74451748+alexlynn013@users.noreply.github.com>
2022-05-30 19:47:22 +0630

    done

--- src/with_ide/Q18HierarchicalDemo.java
+++ src/with_ide/Q18HierarchicalDemo.java
@@ -1,4 +1,48 @@
 package with_ide;
 
-public class Q18HierarchicalDemo {
+class Employee{
+    double salary = 5000;
+
+    void displaySalary(){
+        System.out.println("Employee Salary: Dollor :"+ salary);
+    }
 }
+ class Fulltime extends Employee {
+
+    double hike = 1;
+
+    void increase(){
+        salary = salary + (salary * hike);
+    }
+
+}
+class PartTime extends Employee{
+    double hike = 0.25;
+
+    void increase(){
+        salary = salary + (salary * hike);
+    }
+
+}
+public class Q18HierarchicalDemo{
+    public static void main(String[] args) {
+        Fulltime obj = new Fulltime();
+        PartTime obj1 = new PartTime();
+
+        System.out.println("Salary of a full-time:");
+        obj.displaySalary();
+
+        System.out.println("Salary of a Part-time:");
+        obj1.displaySalary();
+
+        obj.increase();
+        obj1.increase();
+
+        System.out.println("Full-time increasement");
+        obj.displaySalary();
+
+        System.out.println("Part-time increasement");
+        obj1.displaySalary();
+    }
+}
+
